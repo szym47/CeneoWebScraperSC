bdad508caeaf23e3fcd435f3e7eda21e7a090086
Szymon Cebula <szymon.sc.04@gmail.com>
2024-05-13 14:47:12 +0200

    "2024-05-13SC"

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,15 @@
 from app import app
-from flask import render_template
+from flask import render_template,request,redirect,url_for
 
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template("index.html")
 
-@app.route('/extract')
+@app.route('/extract',methods=['POST','GET'])
 def extract():
+    if request.method=='POST':
+        product_id=request.form.get(product_id)
+        return redirect(url_for('product',product_id=product_id))
     return render_template("extract.html")
 
 @app.route('/products')
@@ -18,10 +20,6 @@ def products():
 def about():
     return render_template("about.html")
 
-@app.route('/name/<name>')
-def name(name):
-    return f"Hello, {name}!"
-
-@app.route('/name/<product_id>')
-def product_id(product_id):
+@app.route('/product/<product_id>')
+def product(product_id):
     return render_template("product.html", product_id=product_id)
